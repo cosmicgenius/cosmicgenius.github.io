83d085e5793fcee2dac26f1d3805a0614c0a6e5e
Anthony Wang <40553084+cosmicgenius@users.noreply.github.com>
2020-06-18 16:56:14 -0700

    JS Preamble

---
+++ preamble.js
@@ -0,0 +1,9 @@
+document.write("<link rel=\"stylesheet\" href=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/font-awesome\/4.7.0\/css\/font-awesome.min.css\">");
+document.write("<link type=\"text\/css\" rel=\"stylesheet\" href=\"\/style.css\"\/>");
+
+document.write("<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"\/apple-touch-icon.png\">");
+document.write("<link rel=\"icon\" type=\"image\/png\" sizes=\"32x32\" href=\"\/favicon-32x32.png\">");
+document.write("<link rel=\"icon\" type=\"image\/png\" sizes=\"16x16\" href=\"\/favicon-16x16.png\">");
+document.write("<link rel=\"manifest\" href=\"\/manifest.json\">");
+document.write("<link rel=\"mask-icon\" href=\"\/safari-pinned-tab.svg\" color=\"#5bbad5\">");
+document.write("<meta name=\"theme-color\" content=\"#ffffff\">");
